a36e88e8be77abacee39197b8dad9687415cfcc4
Pythonando <contato.pycursos@gmail.com>
2025-10-05 17:04:14 -0300

    Projeto completo

---
+++ consultas/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Gravacoes, Pergunta, DataTreinamento
+
+# Register your models here.
+
+admin.site.register(Gravacoes)
+admin.site.register(Pergunta)
+admin.site.register(DataTreinamento)
---
+++ usuarios/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Pacientes
+
+# Register your models here.
+
+admin.site.register(Pacientes)
